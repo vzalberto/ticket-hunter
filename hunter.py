505a483eafd7b9b9cd753782b1959da9a0a0c385
Ubuntu <ubuntu@ip-172-31-31-224.us-west-2.compute.internal>
2013-07-30 05:51:32 +0000

    Fixed minor details

--- hunter.py
+++ hunter.py
@@ -3,16 +3,22 @@ import twitter
 execfile('keys.py')
 
 api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
-statuses = api.GetSearch(term='regalaremos boletos ceremonia')
+statuses = api.GetSearch(term='boletos gratis')
 url = 'http://twitter.com/'
+i = 0
 
+api.PostUpdate('Statuses count='+str(len(statuses)))
 for s in statuses:
-	Turl = url+s.user.screen_name+'/status/'+s.id
-	rt = s.text.find('RT ')
-	if rt < 0:		
-		api.PostUpdate(Turl)
-	else:
-		log = open('log.txt', 'a')
-		log.write(url+'\n')
-		log.close()
+    i = i + 1
+    print i
+    Turl = url+s.user.screen_name+'/status/'+str(s.id)
+    rt = s.text.find('RT ')
+    if rt < 0:		
+        api.PostUpdate(Turl)
+    else:
+        log = open('log.txt', 'a')
+        log.write(Turl+'\n')
+        log.close()
+
+
 

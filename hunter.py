a6485bb5daf3dfd42996c14940269ad1fb1310c9
Ubuntu <ubuntu@ip-172-31-31-224.us-west-2.compute.internal>
2013-08-05 06:49:24 +0000

    Integrated send_sms into main script

--- hunter.py
+++ hunter.py
@@ -1,20 +1,23 @@
 import twitter
+from twilio.rest import TwilioRestClient
 
 execfile('keys.py')
+execfile('twilio_auth.py')
 
 api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
 statuses = api.GetSearch(term='boletos gratis')
 url = 'http://twitter.com/'
-i = 0
+print str(len(statuses))+" nuevos tuits"
 
-api.PostUpdate('Statuses count='+str(len(statuses)))
+client = TwilioRestClient(account_sid, auth_token)
+
 for s in statuses:
-    i = i + 1
-    print i
     Turl = url+s.user.screen_name+'/status/'+str(s.id)
+    smsBody = "@"+s.user.screen_name+" "+s.text
     rt = s.text.find('RT ')
     if rt < 0:		
-        api.PostUpdate(Turl)
+        api.PostUpdate("@sereunespejo "+Turl)
+	message = client.sms.messages.create(body=smsBody, to=dest_number, from_=tw_number)
     else:
         log = open('log.txt', 'a')
         log.write(Turl+'\n')
--- send_sms.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import TwilioRestClient
-
-execfile('twilio_auth.py')
-
-client = TwilioRestClient(account_sid, auth_token)
-
-message = client.sms.messages.create(body="2 nuevos tuits", to=dest_number, from_=tw_number)
-print message.sid
-
